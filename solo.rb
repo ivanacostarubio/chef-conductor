48818edf6e096f9b92bfa747cd74176227774c15
Ivan Acosta-Rubio <ivan@bakedweb.net>
2012-12-10 16:38:56 -0430

    changing path for cookbooks

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 # chef-solo -c /etc/chef/solo.rb -j /etc/chef/dna.json
 file_cache_path  "/tmp/chef-solo"
-cookbook_path    "/var/chef-solo/cookbooks"
+cookbook_path    "/root/cookbooks"
 log_level        :info
 log_location     STDOUT
 ssl_verify_mode  :verify_none
